3ab6c2d746a752813eaab9e41b9a471faafa7a5a
AnnaClaire Marley <AnnaClaire.Marley@erg.com>
2025-07-14 17:05:27 -0700

    Added drop down to adjust graph to each season

--- R/mod_seasonal_temp_plot.R
+++ R/mod_seasonal_temp_plot.R
@@ -11,6 +11,13 @@ mod_seasonal_temp_plot_ui <- function(id) {
   ns <- NS(id)
   tagList(
 
+    selectInput(ns("season_choice"),
+                label = "Choose a Season",
+                choices = c("Fall",
+                            "Winter",
+                            "Spring",
+                            "Summer")),
+
     shinycssloaders::withSpinner(highcharter::highchartOutput(ns("plot")))
 
   )
@@ -27,39 +34,74 @@ mod_seasonal_temp_plot_server <- function(id){
     base_yr_start <- 1951
     base_yr_end <- 2000
 
-    # Read in data
-    st_path <- "inst/extdata/seasonal_temp" # path to data
-    obs_raw <- readr::read_csv(file.path(st_path, "seasonal-temperature_fig-1.csv"), skip = 6)
-    fall_proj <- readr::read_csv(file.path(st_path,'conus_avg_fall_temp.csv')) # Average Conus
-    fall_ssps <- vroom::vroom(list.files(path = st_path, pattern = 'avg_son_temp_conus_av_*', full.names = TRUE)) # model averages
+    # Set path
+    st_path <- "inst/extdata/seasonal_temp"
 
-    fall_test <- process_seasons(
-      which_season = "Fall",
-      obs_data = obs_raw,
-      proj_data = fall_proj,
-      ssp_data = fall_ssps,
-      ssp_var = avg_son_temp_f,
-      base_yr_start = base_yr_start,
-      base_yr_end = base_yr_end
+    # Read in observed data
+    obs_raw <- readr::read_csv(file.path(st_path, "seasonal-temperature_fig-1.csv"), skip = 6)
+
+
+
+    seas_proj_adj_out <- reactive({
+
+      # Set variable values depending on which season is selected
+      if (input$season_choice == "Fall") {
+
+        proj_av_pattern <- "fall"
+        proj_all_pattern <- "son"
+        var_name <- "avg_son_temp_f"
+
+      } else if (input$season_choice == "Winter"){
+
+        proj_av_pattern <- "winter"
+        proj_all_pattern <- "djf"
+        var_name <-  "avg_djf_temp_f"
+      } else if (input$season_choice == "Spring"){
+
+        proj_av_pattern <- "spring"
+        proj_all_pattern <- "mam"
+        var_name <- "avg_mam_temp_f"
+
+      } else if (input$season_choice == "Summer"){
+
+        proj_av_pattern <- "summer"
+        proj_all_pattern <- "jja"
+        var_name <-"avg_jja_temp_f"
+
+      }
+
+      # read in the projections dataset for the chosen season
+      proj_av <- readr::read_csv(file.path(st_path, sprintf('conus_avg_%s_temp.csv', proj_av_pattern))) # Average Conus
+      proj_all <- vroom::vroom(list.files(path = st_path, pattern = sprintf('avg_%s_temp_conus_av_*', proj_all_pattern), full.names = TRUE)) # model averages
+
+      # Process the projections indicator
+      seas_proj_adj <- process_seasons(
+        which_season = tools::toTitleCase(proj_av_pattern),
+        obs_data = obs_raw,
+        proj_data = proj_av,
+        ssp_data = proj_all,
+        ssp_var = proj_all[[var_name]],
+        base_yr_start = base_yr_start,
+        base_yr_end = base_yr_end
       )
 
-    ### Create the plot ###
-    st_hc <- create_hc_plot(fall_test,
-                            "Average Fall Temperature in the Contiguous 48 States, 1895–2100",
-                            "Temperature Anomaly (°F)",
-                            "°F")
-
-    output$plot <- highcharter::renderHighchart({
-
-      st_hc
+      return(seas_proj_adj)
 
     })
 
-    # reactive values
-    st_plot <- reactiveVal()
-    st_plot(st_hc)
 
-    return(st_plot)
+
+    ### Create the plot ###
+
+    output$plot <- highcharter::renderHighchart({
+
+      create_hc_plot(seas_proj_adj_out(),
+                              sprintf("Average %s Temperature in the Contiguous 48 States, 1896–2100", input$season_choice),
+                              "Temperature Anomaly (°F)",
+                              "°F")
+
+    })
+
 
   })
 }
