61faa61a517ada84ca0dbc32f1daaf97fd3ef891
AnnaClaire Marley <AnnaClaire.Marley@erg.com>
2025-08-04 16:40:38 -0700

    Added before after slider to av temp map

--- R/mod_av_temp_map.R
+++ R/mod_av_temp_map.R
@@ -11,17 +11,54 @@ mod_av_temp_map_ui <- function(id) {
   ns <- NS(id)
   tagList(
 
-    selectInput(ns("scenario_choice"),
-                label = "Choose a Scenario",
-                choices = c("Observations",
-                            "Low emissions (SSP1-2.6)",
-                            "Intermediate emissions (SSP2-4.5)",
-                            "High emissions (SSP3-7.0)",
-                            "Very high emissions (SSP5-8.5)))")
+    fluidRow(
+
+      column(4,
+
+        selectInput(ns("scenario_choice"),
+                    label = "Choose a Scenario",
+                    choices = c("Observations",
+                                "Low emissions (SSP1-2.6)",
+                                "Intermediate emissions (SSP2-4.5)",
+                                "High emissions (SSP3-7.0)",
+                                "Very high emissions (SSP5-8.5)")
+        )
+
+      ),
+
+      column(4,
+
+        selectInput(ns("scenario_choice_2"),
+                    label = "Choose a Scenario",
+                    choices = c("Observations",
+                                "Low emissions (SSP1-2.6)",
+                                "Intermediate emissions (SSP2-4.5)",
+                                "High emissions (SSP3-7.0)",
+                                "Very high emissions (SSP5-8.5)"),
+                    selected = "Very high emissions (SSP5-8.5)"
+        )
+
+      )
+
     ),
-
-    shinycssloaders::withSpinner(plotOutput(ns("map")))
-
+    # Before after slider
+    tags$script("
+              $(function() {
+    $('#mod_av_temp_map_comparison_slider').beforeAfter({
+        introDelay: 2000,
+        imagePath: 'img/',
+        introDuration: 500,
+        showFullLinks: false
+    })
+                });
+    "),
+    shinycssloaders::withSpinner(
+      tags$div(
+        id = "mod_av_temp_map_comparison_slider",
+        plotOutput(ns("map")),
+        plotOutput(ns("map_2")),
+      )
+    )
   )
 }
 
@@ -31,38 +68,6 @@ mod_av_temp_map_ui <- function(id) {
 mod_av_temp_map_server <- function(id){
   moduleServer(id, function(input, output, session){
     ns <- session$ns
-
-
-# Read in data ------------------------------------------------------------
-
-    clim_divs <- sf::read_sf('inst/extdata/map_boundaries/climate_divisions.geojson') %>% clean_climdiv() # Climate divisions
-    us_states <- sf::read_sf('inst/extdata/map_boundaries/us_states.geojson')
-
-# Bind together observed and projected and prepare for mapping ------------
-
-    all_temps <- rbind(av_temp_map_obs, av_temp_map_mod) %>%
-      dplyr::left_join(clim_divs, by = "climdiv") %>%
-      sf::st_as_sf() %>%
-      dplyr::filter(!is.na(rate_change_100)) %>%
-      rename_scenarios(., TRUE) %>%
-      dplyr::mutate(legend_buckets = cut(rate_change_100, breaks = seq(0, 16, by = 2))) %>%
-      dplyr::mutate(legend_buckets_01 = cut(rate_change_100, breaks = c(-0.1, 0, 0.1, 2))) %>%
-      dplyr::mutate(legend_buckets = ifelse(rate_change_100 <= 2, as.character(legend_buckets_01), as.character(legend_buckets)))
-
-    all_temps$legend_buckets <- as.factor(all_temps$legend_buckets)
-    all_temps$legend_buckets <- forcats::fct_relevel(all_temps$legend_buckets, c(
-      "(-0.1,0]",
-      "(0,0.1]" ,
-      "(0.1,2]",
-      "(2,4]",
-      "(4,6]",
-      "(6,8]",
-      "(8,10]",
-      "(10,12]",
-      "(12,14]",
-      "(14,16]"
-    ))
-
 
 # Make the maps ------------------------------------------------------------
 
@@ -91,8 +96,7 @@ mod_av_temp_map_server <- function(id){
       #  ggplot2::geom_sf(data = which_map, ggplot2::aes(fill = legend_buckets, color = legend_buckets)) +
       ggplot2::scale_fill_manual(values = which_colors, drop = FALSE) +
       #  ggplot2::scale_color_manual(values = temp_colors, drop = FALSE) +
-      ggplot2::facet_wrap(~scenario_title, ncol = 2) +
-      #  ggplot2::geom_sf(data = us_states %>% dplyr::filter(!STUSPS%in%c("AK", "HI", "PR", "AS", "MP", "GU")), color = "black", fill = NA) +
+      ggplot2::geom_sf(data = conus_cln, fill = NA, color = "black") +
       ggplot2::labs(
         title = "Rate of Temperature Change in the United States, 2024–2100",
         fill = "Rate of temperature change\n(°F per century)"
@@ -109,7 +113,7 @@ mod_av_temp_map_server <- function(id){
 
   }
 
-  all_scenarios <- unique(all_temps$scenario)
+  all_scenarios <- unique(av_temp_map_cln_data$scenario)
   names(all_scenarios) <- all_scenarios  # Set names to match values
   all_maps <- lapply(all_scenarios, make_temp_map, all_temps, temp_colors)
 
@@ -124,8 +128,22 @@ mod_av_temp_map_server <- function(id){
                         "Low emissions (SSP1-2.6)" = all_maps$ssp126,
                         "Intermediate emissions (SSP2-4.5)" = all_maps$ssp245,
                         "High emissions (SSP3-7.0)" = all_maps$ssp370,
-                        "Very high emissions (SSP5-8.5)))" = all_maps$ssp585
+                        "Very high emissions (SSP5-8.5)" = all_maps$ssp585
                         )
+    return(which_map)
+
+  })
+
+  output$map_2 <- renderPlot({
+
+    # Map selection to file paths
+    which_map <- switch(input$scenario_choice_2,
+                        "Observations" = all_maps$observed,
+                        "Low emissions (SSP1-2.6)" = all_maps$ssp126,
+                        "Intermediate emissions (SSP2-4.5)" = all_maps$ssp245,
+                        "High emissions (SSP3-7.0)" = all_maps$ssp370,
+                        "Very high emissions (SSP5-8.5)" = all_maps$ssp585
+    )
     return(which_map)
 
   })
