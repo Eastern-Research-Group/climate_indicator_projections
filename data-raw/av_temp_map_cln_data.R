8fa3b929710b952d7b2b2b55601418dfc3f0e659
AnnaClaire Marley <AnnaClaire.Marley@erg.com>
2025-08-05 14:04:23 -0700

    Forgot to filter to years greater than 2024, which added back in the legend buckets

--- data-raw/av_temp_map_cln_data.R
+++ data-raw/av_temp_map_cln_data.R
@@ -31,6 +31,7 @@ av_temp_map_mod <- av_temp_map_mod_raw %>%
             model_range = FALSE,
             for_maps = TRUE) %>%
   dplyr::ungroup() %>%
+  dplyr::filter(year >= min_yr) %>% # Make everything from 2024
   dplyr::group_by(climdiv, scenario) %>%
   # Calculate the rate of change of the anomalies
   dplyr::mutate(rate_change = lm(anomaly ~ year)$coefficients[[2]]) %>%
@@ -44,7 +45,7 @@ av_temp_map_cln_data <- rbind(av_temp_map_obs, av_temp_map_mod) %>%
   sf::st_as_sf() %>%
   dplyr::filter(!is.na(rate_change_100)) %>%
   rename_scenarios(., TRUE) %>%
-  dplyr::mutate(legend_buckets = cut(rate_change_100, breaks = seq(0, 12, by = 2))) %>%
+  dplyr::mutate(legend_buckets = cut(rate_change_100, breaks = seq(0, 16, by = 2))) %>%
   dplyr::mutate(legend_buckets_01 = cut(rate_change_100, breaks = c(-0.1, 0, 0.1, 2))) %>%
   dplyr::mutate(legend_buckets = ifelse(rate_change_100 <= 2, as.character(legend_buckets_01), as.character(legend_buckets)))
 
@@ -58,7 +59,9 @@ av_temp_map_cln_data$legend_buckets <- forcats::fct_relevel(
     "(4,6]",
     "(6,8]",
     "(8,10]",
-    "(10,12]"
+    "(10,12]",
+    "(12,14]",
+    "(14,16]"
 ))
 
 usethis::use_data(av_temp_map_cln_data, overwrite = TRUE)
