3d61b03354309d1291011503e986f2268578a743
AnnaClaire Marley <AnnaClaire.Marley@erg.com>
2025-08-05 15:34:14 -0700

    Create function for making ggplot map

---
+++ R/fct_create_static_map.R
@@ -0,0 +1,41 @@
+#' create_static_map
+#'
+#' @param which_scenario String of the scenario to filter to (e.g. ssp585)
+#' @param map_data The geodataframe to map
+#' @param which_colors List of colors to use for each legend bucket
+#' @param title String of the plot title
+#' @param legend_title String of the legend title
+#' @param state_color Color to have the state outline be
+#'
+#' @description Create a static map for the indicator
+#'
+#' @return A ggplot map
+#'
+#' @noRd
+create_static_map <- function(which_scenario, map_data, which_colors, title, legend_title, state_color = "black"){
+
+    # Filter to the scenario to map
+    which_map <- map_data %>%
+      dplyr::filter(scenario == which_scenario)
+
+    # Create the map
+    out_map <- ggplot2::ggplot() +
+      ggplot2::geom_sf(data = which_map, ggplot2::aes(fill = legend_buckets), color = "#88807F") +
+      ggplot2::scale_fill_manual(values = which_colors, drop = FALSE) +
+      ggplot2::geom_sf(data = conus_cln, fill = NA, color = state_color) +
+      ggplot2::labs(
+        title = title,
+        fill = legend_title
+      ) +
+      ggthemes::theme_map() +
+      ggplot2::theme(
+        text = ggplot2::element_text(size = 12),
+        plot.title = ggplot2::element_text(size = 14, hjust = 0.5),
+        #   legend.key.width = ggplot2::unit(2, 'cm'),
+        legend.position = "bottom"
+      )
+
+    return(out_map)
+
+
+}
