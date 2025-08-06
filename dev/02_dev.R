f03aa70ad8a5c8a1e665f185edf2583b3c7d37c9
AnnaClaire Marley <AnnaClaire.Marley@erg.com>
2025-08-05 17:01:27 -0700

    Changes

--- dev/02_dev.R
+++ dev/02_dev.R
@@ -17,16 +17,60 @@
 ## Amend DESCRIPTION with dependencies read from package code parsing
 ## install.packages('attachment') # if needed.
 attachment::att_amend_desc()
+usethis::use_pipe()
 
 ## Add modules ----
 ## Create a module infrastructure in R/
-golem::add_module(name = "name_of_module1", with_test = TRUE) # Name of the module
-golem::add_module(name = "name_of_module2", with_test = TRUE) # Name of the module
+# Average Temperature
+golem::add_module(name = "av_temp_plot", with_test = TRUE)
+golem::add_module(name = "av_temp_map", with_test = TRUE)
+# Seasonal Temperature
+golem::add_module(name = "seasonal_temp_plot", with_test = TRUE)
+golem::add_module(name = "seasonal_temp_map", with_test = TRUE)
+# Growing Season
+golem::add_module(name = "grow_season_plot", with_test = TRUE)
+golem::add_module(name = "grow_season_map", with_test = TRUE)
+# Annual Total Precipitation
+golem::add_module(name = "precip_plot", with_test = TRUE)
+
+# Sea Level Rise
+golem::add_module(name = "slr_plot", with_test = TRUE)
+golem::add_module(name = "slr_map", with_test = TRUE)
+
+golem::add_module(name = "sst_plot", with_test = TRUE) # Sea surface temperature
+golem::add_module(name = "ocean_acidity_plot", with_test = TRUE) # Ocean Acidity
+golem::add_module(name = "arctic_sea_ice_plot", with_test = TRUE) # Arctic Sea Ice
+golem::add_module(name = "snow_cover_plot", with_test = TRUE) # Snow Cover
+golem::add_module(name = "ghg_conc_plot", with_test = TRUE) # GHG concentrations
 
 ## Add helper functions ----
 ## Creates fct_* and utils_*
 golem::add_fct("helpers", with_test = TRUE)
-golem::add_utils("helpers", with_test = TRUE)
+golem::add_fct("model_processing", with_test = TRUE)
+golem::add_fct("create_hc_plot", with_test = TRUE)
+golem::add_fct("create_static_map", with_test = TRUE)
+golem::add_utils("calc_anom", with_test = TRUE)
+golem::add_utils("calc_model_range", with_test = TRUE)
+golem::add_utils("calc_diff_avs", with_test = TRUE)
+golem::add_utils("adjust_anomaly", with_test = TRUE)
+golem::add_utils("rename_scenarios", with_test = TRUE)
+golem::add_utils("read_ssps", with_test = TRUE)
+golem::add_utils("process_seasons", module = "seasonal_temp_plot", with_test = TRUE)
+golem::add_utils("rename_stations", module = "ocean_acidity_plot", with_test = TRUE)
+golem::add_utils("add_hind_data", module = "ocean_acidity_plot", with_test = TRUE)
+golem::add_utils("clean_oa_obs", module = "ocean_acidity_plot", with_test = TRUE)
+golem::add_utils("process_oa_station", module = "ocean_acidity_plot", with_test = TRUE)
+golem::add_utils("process_sea_ice", module = "arctic_sea_ice_plot", with_test = TRUE)
+golem::add_utils("clean_sc_data", module = "snow_cover_plot", with_test = TRUE)
+golem::add_utils("process_sc", module = "snow_cover_plot", with_test = TRUE)
+golem::add_utils("create_sc_seas", module = "snow_cover_plot", with_test = TRUE)
+golem::add_utils("create_slr_plot", module = "slr_plot", with_test = TRUE)
+golem::add_utils("cln_ghg_obs", module = "ghg_conc_plot", with_test = TRUE)
+golem::add_utils("create_ghg_plot", module = "ghg_conc_plot", with_test = TRUE)
+golem::add_utils("clean_climdiv", with_test = TRUE)
+golem::add_utils("calc_total_change", with_test = TRUE)
+golem::add_utils("clean_seas_temp_map_mod", with_test = TRUE)
+
 
 ## External resources
 ## Creates .js and .css files at inst/app/www
@@ -38,7 +82,70 @@ golem::add_any_file("file.json")
 
 ## Add internal datasets ----
 ## If you have data in your package
-usethis::use_data_raw(name = "my_dataset", open = FALSE)
+# Arctic Sea Ice: Plots
+usethis::use_data_raw(name = "asi_plot_cln_data", open = rlang::is_interactive())
+
+# Average temperature: Plots
+usethis::use_data_raw(name = "av_temp_plot_cln_data", open = rlang::is_interactive())
+
+# Average temperature: Maps
+usethis::use_data_raw(name = "av_temp_map_obs", open = rlang::is_interactive())
+usethis::use_data_raw(name = "av_temp_map_mod", open = rlang::is_interactive())
+usethis::use_data_raw(name = "av_temp_map_cln_data", open = rlang::is_interactive())
+
+# Greenhouse Gas Concentrations: Plots
+usethis::use_data_raw(name = "ghg_conc_plot_obs_co2", open = rlang::is_interactive())
+usethis::use_data_raw(name = "ghg_conc_plot_obs_ch4", open = rlang::is_interactive())
+usethis::use_data_raw(name = "ghg_conc_plot_obs_n2o", open = rlang::is_interactive())
+usethis::use_data_raw(name = "ghg_conc_plot_mod_av", open = rlang::is_interactive())
+
+# Growing Season: Plots
+usethis::use_data_raw(name = "grow_seas_plot_cln_data", open = rlang::is_interactive())
+usethis::use_data_raw(name = "grow_seas_map_cln_data", open = rlang::is_interactive())
+
+# Ocean acidity: Plots
+usethis::use_data_raw(name = "oa_plot_obs", open = rlang::is_interactive())
+usethis::use_data_raw(name = "oa_plot_mod_av", open = rlang::is_interactive())
+usethis::use_data_raw(name = "oa_plot_mod_all", open = rlang::is_interactive())
+usethis::use_data_raw(name = "oa_plot_cln_data_hawaii", open = rlang::is_interactive())
+usethis::use_data_raw(name = "oa_plot_cln_data_canary_islands", open = rlang::is_interactive())
+usethis::use_data_raw(name = "oa_plot_cln_data_bermuda", open = rlang::is_interactive())
+usethis::use_data_raw(name = "oa_plot_cln_data_cariaco", open = rlang::is_interactive())
+
+# Seasonal Temperature: Plots
+usethis::use_data_raw(name = "seas_temp_plot_obs", open = rlang::is_interactive())
+usethis::use_data_raw(name = "seas_temp_plot_cln_data_fall", open = rlang::is_interactive())
+usethis::use_data_raw(name = "seas_temp_plot_cln_data_winter", open = rlang::is_interactive())
+usethis::use_data_raw(name = "seas_temp_plot_cln_data_spring", open = rlang::is_interactive())
+usethis::use_data_raw(name = "seas_temp_plot_cln_data_summer", open = rlang::is_interactive())
+
+# Seasonal Temperature: Maps
+usethis::use_data_raw(name = "seas_temp_map_obs", open = rlang::is_interactive())
+usethis::use_data_raw(name = "seas_temp_map_cln_data_fall", open = rlang::is_interactive())
+usethis::use_data_raw(name = "seas_temp_map_cln_data_winter", open = rlang::is_interactive())
+usethis::use_data_raw(name = "seas_temp_map_cln_data_spring", open = rlang::is_interactive())
+usethis::use_data_raw(name = "seas_temp_map_cln_data_summer", open = rlang::is_interactive())
+
+# Sea Level Rise: Plots
+usethis::use_data_raw(name = "slr_plot_obs", open = rlang::is_interactive())
+usethis::use_data_raw(name = "slr_plot_obs_csiro_bounds", open = rlang::is_interactive())
+usethis::use_data_raw(name = "slr_plot_mod_all", open = rlang::is_interactive())
+
+# Snow Cover: Plots
+usethis::use_data_raw(name = "sc_plot_winter", open = rlang::is_interactive())
+usethis::use_data_raw(name = "sc_plot_spring", open = rlang::is_interactive())
+usethis::use_data_raw(name = "sc_plot_summer", open = rlang::is_interactive())
+usethis::use_data_raw(name = "sc_plot_fall", open = rlang::is_interactive())
+usethis::use_data_raw(name = "sc_plot_annual", open = rlang::is_interactive())
+
+# Sea Surface Temperature: Plots
+usethis::use_data_raw(name = "sst_plot_cln_data", open = rlang::is_interactive())
+
+# Geospatial Files
+usethis::use_data_raw(name = "clim_div_cln", open = rlang::is_interactive())
+usethis::use_data_raw(name = "conus_cln", open = rlang::is_interactive())
+
+
 
 ## Tests ----
 ## Add one line by test you want to create
