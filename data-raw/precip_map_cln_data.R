115c42099c906c82a1f5fa29ab9de8273824b93c
AnnaClaire Marley <AnnaClaire.Marley@erg.com>
2025-08-12 12:14:48 -0700

    Updated with the 1951-2000 observed baseline, added the projections data, and am playing around with the facet wrap version of the map

--- data-raw/precip_map_cln_data.R
+++ data-raw/precip_map_cln_data.R
@@ -1,29 +1,48 @@
 ## code to prepare `precip_map_cln_data` dataset goes here
 
 # Set the years -----------------------------------------------------------
+min_yr <- 2024 # first year to start the rate of change on - the year after the end of the observed data
+base_yr_start <- 1951
+base_yr_end <- 2000
 
 # Read in the data --------------------------------------------------------
-precip_map_obs_raw <- readr::read_csv(file.path(config::get("precip_path"), "precipitation_fig-3.csv"), skip = 6)
+precip_map_obs_raw <- readr::read_csv(file.path(config::get("precip_path"), "precipitation_fig-3_1951-2000_baseline.csv"))
+precip_map_proj_raw <- readr::read_csv(file.path(config::get("precip_path"), "climdiv_TotalAnnualPr.csv"))
 
 # Clean the observed data -------------------------------------------------
 precip_map_obs_cln <- precip_map_obs_raw %>%
   # Clean up the names
   janitor::clean_names() %>%
-  dplyr::select(-precipitation_change_1925_2000_denominator) %>%
   dplyr::rename(climdiv = climate_division_id) %>%
-  dplyr::rename(perc_change = precipitation_change_1901_2000_denominator)  %>%
-  dplyr::mutate(scenario = "observed") %>%
-  # Make percent change numeric
-  dplyr::mutate(perc_change = stringr::str_remove(perc_change, "%")) %>%
-  dplyr::mutate(perc_change = as.numeric(perc_change)) %>%
-  dplyr::filter(!is.na(perc_change))
-
+  dplyr::rename(perc_change = precipitation_change_1951_2000_denominator)  %>%
+  dplyr::mutate(scenario = "observed")
 
 # Clean the model average -------------------------------------------------
 
+# calculate the average for each scenario 1951-2000
+proj_51_00_av <- precip_map_proj_raw %>%
+  sf::st_drop_geometry() %>%
+  dplyr::filter(scenario != "nclimgrid") %>%
+  dplyr::filter(!is.na(total_pr)) %>%
+  dplyr::filter(year >= base_yr_start & year <= base_yr_end) %>%
+  dplyr::group_by(climdiv, scenario) %>%
+  dplyr::summarize(base_precip = mean(total_pr))
+
+precip_map_proj_cln <- precip_map_proj_raw %>%
+  dplyr::filter(scenario != "nclimgrid") %>%
+  dplyr::left_join(proj_51_00_av, by = c("climdiv", "scenario")) %>%
+  dplyr::filter(year >= min_yr) %>%
+  dplyr::ungroup() %>%
+  dplyr::group_by(climdiv, scenario) %>%
+  dplyr::mutate(rate_change = lm(total_pr ~ year)$coefficients[[2]]) %>%
+  dplyr::mutate(ttl_change = rate_change*(2100-min_yr)) %>%
+  dplyr::mutate(perc_change = (ttl_change/base_precip)*100) %>%
+  dplyr::select(climdiv, scenario, perc_change)
+
 # Combine and process -----------------------------------------------------
 
-precip_map_cln_data <- precip_map_obs_cln %>%  # FIX THIS WHEN GET PROJECTIONS DATA
+precip_map_cln_data <- precip_map_obs_cln %>%
+  rbind(precip_map_proj_cln) %>%
   rename_scenarios(., TRUE) %>%
   # Create legend buckets
   dplyr::mutate(legend_buckets = cut(perc_change,
@@ -32,5 +51,40 @@ precip_map_cln_data <- precip_map_obs_cln %>%  # FIX THIS WHEN GET PROJECTIONS D
   dplyr::left_join(clim_div_cln, by = "climdiv") %>%
   sf::st_as_sf()
 
+
+
+
+precip_colors <- c(
+  "(-30,-20]" = "#D9B651",
+  "(-20,-10]" = "#E6CD8F",
+  "(-10,-2]" = "#EFE2C7",
+  "(-2,2]" = "#C7C7C7",
+  "(2,10]" = "#91BBE1",
+  "(10,20]" = "#4781D2",
+  "(20,30]" = "#3466AC"
+)
+
+# Create the map
+out_map <- ggplot2::ggplot() +
+  ggplot2::geom_sf(data = precip_map_cln_data, ggplot2::aes(fill = legend_buckets), color = "#88807F") +
+  ggplot2::facet_wrap(~scenario_title) +
+  ggplot2::scale_fill_manual(values = precip_colors, drop = FALSE) +
+  ggplot2::geom_sf(data = conus_cln, fill = NA, color = "black") +
+  ggplot2::labs(
+    title = "Change in Precipitation in the United States",
+    fill = "Percent change in precipitation"
+  ) +
+  ggthemes::theme_map() +
+  ggplot2::theme(
+    text = ggplot2::element_text(size = 12),
+    plot.title = ggplot2::element_text(size = 14, hjust = 0.5),
+    #   legend.key.width = ggplot2::unit(2, 'cm'),
+    legend.position = "bottom"
+  )
+out_map
+
+ggplot2::ggsave(filename = "new_precip_maps.jpeg",
+                plot = out_map,
+                path = file.path(config::get("precip_path")))
 
 usethis::use_data(precip_map_cln_data, overwrite = TRUE)
