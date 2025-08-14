331461c8bd7113ebe0bc9adf6035f758f62ab2e8
AnnaClaire Marley <AnnaClaire.Marley@erg.com>
2025-08-14 10:16:56 -0700

    ui functions

---
+++ R/fct_create_static_map_ui.R
@@ -0,0 +1,85 @@
+#' create_static_map_ui
+#'
+#' @param ns name space
+#' @param obs_dates string of dates for observed data title
+#' @param proj_dates string of dates for projected data title
+#' @param title title of map
+#'
+#' @description A fct function that creates the map UI
+#'
+#' @return The return value, if any, from executing the function.
+#'
+#' @noRd
+create_static_map_ui <- function(ns, obs_dates, proj_dates, title, seasonal_temp) {
+
+  map_id <- ns("map_comparison")
+
+  tagList(
+
+    if (seasonal_temp) {
+
+      radioButtons(
+        inputId = ns("seasonRadioButtons"),
+        label = "Choose a season:",
+        choices = c("Winter", "Spring", "Summer", "Fall"),
+        selected = "Fall",
+        inline = TRUE
+      )
+
+    }
+
+    fluidRow(
+
+      column(5,
+
+             selectInput(ns("scenario_choice"),
+                         label = "Scenario 1",
+                         choices = c(sprintf("Observations, %s", obs_dates),
+                                     sprintf("Low emissions (SSP1-2.6), %s", proj_dates),
+                                     sprintf("Intermediate emissions (SSP2-4.5), %s", proj_dates),
+                                     sprintf("High emissions (SSP3-7.0), %s", proj_dates),
+                                     sprintf("Very high emissions (SSP5-8.5), %s", proj_dates)),
+                         width="500px"
+             )
+
+      ),
+
+      column(5,
+
+             selectInput(ns("scenario_choice_2"),
+                         label = "Scenario 2",
+                         choices = c(sprintf("Observations, %s", obs_dates),
+                                     sprintf("Low emissions (SSP1-2.6), %s", proj_dates),
+                                     sprintf("Intermediate emissions (SSP2-4.5), %s", proj_dates),
+                                     sprintf("High emissions (SSP3-7.0), %s", proj_dates),
+                                     sprintf("Very high emissions (SSP5-8.5), %s", proj_dates)),
+                         selected = sprintf("Very high emissions (SSP5-8.5), %s", proj_dates),
+                         width="500px"
+             )
+
+      )
+
+    ),
+
+    # Before after slider
+    tags$script(sprintf("
+              $(function() {
+    $('#%s').beforeAfter({
+        introDelay: 2000,
+        imagePath: 'img/',
+        introDuration: 500,
+        showFullLinks: false
+    })
+                });
+    ", map_id)),
+    tags$p(title, class="title"),
+    shinycssloaders::withSpinner(
+      tags$div(
+        id = map_id,
+        plotOutput(ns("map"), width = "600px", height = "600px"),
+        plotOutput(ns("map_2"), width = "600px", height = "600px"),
+      )
+    )
+
+  )
+}
---
+++ R/fct_render_page.R
@@ -0,0 +1,172 @@
+render_tab_option <- function(
+    id,
+    name,
+    active=FALSE
+) {
+  return(
+    tags$li(
+      tags$a(
+        name,
+        href="#",
+        id=id,
+        class=ifelse(active, "active", "")
+      )
+    )
+  )
+}
+
+render_indicator_page <- function(
+    title,
+    maps=NULL,
+    timeseries=NULL,
+    summary=NULL,
+    tech_doc=NULL
+) {
+
+  if (is.null(maps) & is.null(timeseries)) {
+    stop("Must provide at least one of Maps or Timeseries")
+  }
+
+  panels <- list()
+
+  idx <- 1
+
+  if (!is.null(maps)) {
+    panels[[idx]] <- tabPanel("Maps", maps)
+    idx <- idx + 1
+  }
+
+  if (!is.null(timeseries)) {
+    panels[[idx]] <-
+      tabPanel("Timeseries Graphs", timeseries)
+    idx <- idx + 1
+  }
+
+  if (!is.null(summary)) {
+    panels[[idx]] <-
+      tabPanel("Summary", summary)
+    idx <- idx + 1
+  }
+
+  if (!is.null(timeseries)) {
+    panels[[idx]] <-
+      tabPanel("Technical Documentation", tech_doc)
+    idx <- idx + 1
+  }
+
+  return(
+    tagList(
+      tags$h2(title),
+      do.call(tabsetPanel, panels)
+    )
+
+  )
+
+}
+
+
+# render_indicator_page <- function(
+#     prefix,
+#     title,
+#     maps=NULL,
+#     timeseries=NULL
+#   ) {
+#
+#   if (is.null(maps) & is.null(timeseries)) {
+#     stop("Must provide at least one of Maps or Timeseries")
+#   }
+#
+#   navlist <- tagList()
+#
+#
+#   if (!is.null(maps)) {
+#     navlist <- tagAppendChild(navlist,
+#       render_tab_option(
+#         id=paste0(prefix, "-tab-maps"),
+#         name="Maps",
+#         active=TRUE
+#       )
+#     )
+#   }
+#
+#   if (!is.null(timeseries)) {
+#     navlist <- tagAppendChild(navlist,
+#       render_tab_option(
+#         id=paste0(prefix, "-tab-timeseries"),
+#         name="Timeseries Graphs",
+#         active=is.null(timeseries)
+#       )
+#     )
+#   }
+#
+#   return(
+#     htmlTemplate(
+#       app_sys("app/www/indicator_mod.html"),
+#       title=title,
+#       navlist=tagList(navlist),
+#       prefix=prefix,
+#       maps=ifelse(is.null(maps), "", maps),
+#       timeseries=ifelse(is.null(timeseries), "", timeseries),
+#     )
+#   )
+#
+# }
+
+render_map_page <- function(
+  map,
+  data_source="XXXX",
+  caption="Lorem ipsum dolor sit amet..."
+) {
+  return(
+    htmlTemplate(
+      app_sys("app/www/maps_mod.html"),
+      map=map,
+      data_source=data_source,
+      caption=caption
+    )
+  )
+}
+
+render_timeseries_page <- function(
+    title,
+    timeseries,
+    data_source="XXXX",
+    caption="Lorem ipsum dolor sit amet..."
+) {
+  return(
+    htmlTemplate(
+      app_sys("app/www/timeseries_mod.html"),
+      title=title,
+      timeseries=timeseries,
+      data_source=data_source,
+      caption=caption
+    )
+  )
+}
+
+render_summary <- function(
+    background="Some background",
+    key_points="Some key points",
+    sources_and_methods="Some sources"
+) {
+  return(
+    htmlTemplate(
+      app_sys("app/www/summary_mod.html"),
+      background=background,
+      key_points=key_points,
+      sources_and_methods=sources_and_methods
+    )
+  )
+}
+
+render_tech_doc <- function(
+    tech_doc="Some text goes here..."
+) {
+  return(
+    htmlTemplate(
+      app_sys("app/www/techdoc_mod.html"),
+      tech_doc=tech_doc
+    )
+  )
+}
+
