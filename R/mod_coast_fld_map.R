abd3f41bb158e92dbad9ae5daa5183e07e1c255a
AnnaClaire Marley <AnnaClaire.Marley@erg.com>
2025-08-21 18:23:49 -0400

    Added dummy data for pop up plot

--- R/mod_coast_fld_map.R
+++ R/mod_coast_fld_map.R
@@ -10,12 +10,11 @@
 mod_coast_fld_map_ui <- function(id) {
   ns <- NS(id)
 
-  return(
       render_map_page(
         map=
           shinycssloaders::withSpinner(
             tagList(
-              tags$p("Average Number of Coastal Flood Days per Year, 1950-2100", class="title"),
+              tags$p("Average Number of Coastal Flood Days per Year, 1950-2099", class="title"),
               tags$p("Observed period: 1950s to 2010s | Projected period: 2020s to 2090s", class="subtitle"),
               tags$style(HTML("
                       .irs-grid-text {
@@ -28,6 +27,7 @@ mod_coast_fld_map_ui <- function(id) {
                         font-size: 14px;
                       }
                       ")),
+              # Decade slider
               sliderInput(inputId = ns("decade"),
                           label = "Select Decade:",
                           min = 1950,
@@ -38,15 +38,43 @@ mod_coast_fld_map_ui <- function(id) {
                           sep = "",
                           post = "s",
                           width = "100%"),
+              # Leaflet map
               leaflet::leafletOutput(
                 ns("map"),
                 width = "100%",
                 height = 800
+              ),
+              # Inset plot
+              absolutePanel(
+                id = ns("overlay"),
+                class = "panel panel-default map_overlay",
+                top = 90,
+                right = 40,
+                width = 440,
+                style="display:none;", # Start with it hidden, so it doesn't appear without the map
+                fixed=TRUE,
+                draggable = TRUE,
+                height = "auto",
+                tagList(
+                  tags$div(
+                    span(h4("Please select a location to view coastal flooding plot.")),
+                    id=ns("no_data_selected")
+                  ),
+                  tags$div(
+                    tagList(
+                      h5(textOutput(ns("plot_title"))),
+                      highcharter::highchartOutput(ns("plot"))
+                    ),
+                    id=ns("graph_container"),
+                    style="display:none"
+                  )
+
+                )
               )
-            )
+            ),
           )
       )
-    )
+
 
 }
 
@@ -106,7 +134,7 @@ mod_coast_fld_map_server <- function(id){
         leaflet::clearMarkers() %>%
         # Lower sea level rise
         leaflet::addCircleMarkers(data = lower_slr_filt,
-                                  layerId =  ~paste0(station_name, "Lower SLR"),
+                                  layerId =  ~paste0(station_name, " Lower SLR"),
                                   radius = 6,
                                   stroke = TRUE,
                                   fillOpacity = 1,
@@ -125,6 +153,61 @@ mod_coast_fld_map_server <- function(id){
                                   color = ~pal(fld_days_pdec)
         )
     })
+
+
+    # Inset Plot --------------------------------------------------------------
+
+    city_selected <- reactiveVal(NULL)
+
+    observeEvent(input$map_bounds, {
+      # ONce the map is loaded (we know it is loaded if we get a "Map bounds" event)
+      # Then show the side overlay
+      shinyjs::show("overlay")
+    })
+
+    # Observe click event
+    observeEvent(input$map_marker_click, {
+
+      # Remove type from the ID
+      remove_list <- c(" Lower SLR", " Higher SLR")
+      pattern <- paste(remove_list, collapse = "|")
+
+      click <- stringr::str_remove_all(input$map_marker_click$id, pattern)
+
+      city_selected(click)
+
+    })
+
+    observe({
+      if (is.null(city_selected())) {
+        shinyjs::show("no_data_selected")
+        shinyjs::hide("graph_container")
+      } else {
+        shinyjs::show("graph_container")
+        shinyjs::hide("no_data_selected")
+      }
+    })
+
+    # high chart title
+    output$plot_title <- renderText({
+      paste0(city_selected(), " Coastal Flooding Days")
+    })
+
+
+    # high chart to render
+    output$plot <- highcharter::renderHighchart({
+
+      # Filter here
+      ## IF city_Selected() is null, that means nothing has been selected.
+
+      create_slr_plot(slr_plot_obs,
+                      slr_plot_mod_all,
+                      slr_plot_obs_csiro_bounds,
+                      FALSE)
+
+
+    })
+
 
   })
 }
