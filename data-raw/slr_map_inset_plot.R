45be0a693b2366b52b968b10c1375c5225d5d0b2
AnnaClaire Marley <AnnaClaire.Marley@erg.com>
2025-08-27 12:39:42 -0400

    Initial draft to develop the inset map of slr for the map

---
+++ data-raw/slr_map_inset_plot.R
@@ -0,0 +1,51 @@
+## code to prepare `slr_map_inset_plot` dataset goes here
+
+# Read in the data --------------------------------------------------------
+slr_map_obs_stat_raw <- readr::read_csv(file.path(config::get("slr_path"), "9414290_meantrend.csv"), skip = 5)
+slr_map_mod_stat_raw <- readr::read_csv(file.path(config::get("slr_path"), "SLR_TF U.S. Sea Level Projections.csv"), skip = 17)
+
+# Clean the observed data -------------------------------------------------
+slr_map_obs_ann <- slr_map_obs_stat_raw %>%
+  janitor::clean_names() %>%
+  dplyr::filter(year >= 1960) %>%
+  dplyr::mutate(monthly_msl = measurements::conv_unit(monthly_msl, "m", "in")) %>%
+  dplyr::group_by(year) %>%
+  dplyr::summarize(rsl = mean(monthly_msl))
+
+rsl_1960 <- slr_map_obs_ann %>%
+  dplyr::filter(year == 1960) %>%
+  dplyr::pull(rsl)
+
+slr_map_obs_stat <- slr_map_obs_ann %>%
+  dplyr::mutate(rsl_adj = rsl - rsl_1960)
+
+# Clean the model average -------------------------------------------------
+
+slr_map_mod_stat_cln <- slr_map_mod_stat_raw %>%
+  clean_slr_mod_data() %>% # Initial clean of the modeled data
+  dplyr::filter(noaa_name == "San Francisco") %>%
+  dplyr::select(-lat, -long) %>%
+  dplyr::filter(scenario %in% c("0.5 - MED", "1.0 - MED"))
+
+# Align projected with observed -------------------------------------------
+
+slr_map_mod_stat_adj <- chain_slr_data(
+  obs_data = slr_map_obs_stat,
+  mod_data = slr_map_mod_stat_cln,
+  obs_col = rsl_adj)
+
+# Combine and process -----------------------------------------------------
+
+slr_map_inset_plot <- slr_map_obs_stat %>%
+  dplyr::rename(slr_in = rsl_adj) %>%
+  dplyr::mutate(scenario = "observed") %>%
+  dplyr::select(-rsl) %>%
+  rbind(slr_map_mod_stat_adj)
+
+
+ggplot2::ggplot(slr_map_inset_plot, ggplot2::aes(x = year, y = slr_in)) +
+  ggplot2::geom_line(ggplot2::aes(color=scenario))
+
+
+
+usethis::use_data(slr_map_inset_plot, overwrite = TRUE)
