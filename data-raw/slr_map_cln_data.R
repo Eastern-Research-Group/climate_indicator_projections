98e5e734026858ddcbdfb26f0efe16a72886696a
AnnaClaire Marley <AnnaClaire.Marley@erg.com>
2025-09-03 11:32:18 -0700

    Cleaned up analysis process

--- data-raw/slr_map_cln_data.R
+++ data-raw/slr_map_cln_data.R
@@ -4,35 +4,21 @@
 slr_map_obs_raw <- readr::read_csv(file.path(config::get("slr_path"), "sea-level_fig-2.csv"), skip = 6)
 slr_map_mod_raw <- readr::read_csv(file.path(config::get("slr_path"), "SLR_TF U.S. Sea Level Projections.csv"), skip = 17)
 
-# Clean and process -------------------------------------------------------
-
-# Observed data
+# Clean the observed data -------------------------------------------------
 slr_map_obs <- slr_map_obs_raw %>%
   janitor::clean_names() %>%
   dplyr::mutate(scenario = "observed") %>%
-  dplyr::select(-state)
+  dplyr::select(-state) %>%
+  # rename galveston bay entrance to just galveston
+  dplyr::mutate(station_name = ifelse(station_name == "Galveston Bay Entrance", "Galveston", station_name))
 
-# modeled data
+# Clean the model average -------------------------------------------------
+
 slr_map_mod_cln <- slr_map_mod_raw %>%
   clean_slr_mod_data() %>% # Initial clean of the modeled data
   dplyr::filter(noaa_name != "GMSL") %>% # don't need GMSL
   # Make a column of station names that will match the observations station names
-  dplyr::mutate(station_name = dplyr::case_when(
-    noaa_name == "Baltimore, Fort McHenry, Patapsco River" ~ "Baltimore",
-    noaa_name == "Beaufort, Duke Marine Lab" ~ "Beaufort",
-    noaa_name == "Charleston, Cooper River Entrance" ~ "Charleston",
-    noaa_name == "Freeport" ~ "Freeport Harbor",
-    noaa_name == "Hilo, Hilo Bay, Kuhio Bay" ~ "Hilo",
-    noaa_name == "Kahului, Kahului Harbor" ~ "Kahului",
-    noaa_name == "Kwajalein, Marshall Islands" ~ "Kwajalein",
-    noaa_name == "Mayport (Bar Pilots Dock)" ~ "Mayport",
-    noaa_name == "San Diego, San Diego Bay" ~ "San Diego",
-    noaa_name == "Skagway, Taiya Inlet" ~ "Skagway",
-    noaa_name == "St. Petersburg, Tampa Bay" ~ "St. Petersburg",
-    noaa_name == "Virginia Key, Biscayne Bay" ~ "Virginia Key",
-    noaa_name == "Wake Island, Pacific Ocean" ~ "Wake Island",
-    TRUE ~ noaa_name
-  )) %>%
+  align_slr_station_names() %>%
   dplyr::filter(year > 2005) %>%
   dplyr::group_by(station_name, scenario) %>%
   dplyr::mutate(rate_change = lm(slr_in ~ year)$coefficients[[2]]) %>%
@@ -45,25 +31,17 @@ slr_map_mod_cln <- slr_map_mod_raw %>%
   # fix the points in marshall islands and wake island so that they show up in the same view on leaflet
   dplyr::mutate(long = ifelse(long > 0, long - 360, long))
 
-## TODO: Do I need to adjust for NOAA 2005 offset
-# dplyr::mutate(slr_in = slr_in + noaa_2005)
-
-## TODO: observed data is from 1960 to 2023 but can only use projections that start at 2020 or 2030
-
 # Filter to just the stations that are in both
 obs_stations <- unique(slr_map_obs$station_name)
 mod_stations <- unique(slr_map_mod_cln$station_name)
 wanted_stations <- intersect(obs_stations, mod_stations)
 
-## TODO: MEED TO DECIDE WHAT TO DO HERE:
-#"Galveston Bay Entrance" / "Galveston Pleasure Pier" & "Galveston Pier 21"
-
+## TODO: MEED TO DECI
 
 # Combine and final processing --------------------------------------------
 
 slr_map_cln_data <- rbind(slr_map_obs, slr_map_mod_cln) %>%
   dplyr::filter(station_name %in% wanted_stations) %>%  #filter to just stations that have both modeled and observed data
-  sf::st_as_sf(coords = c("long", "lat"), crs = 4326) # make geospatial
-
+  sf::st_as_sf(coords = c("long", "lat"), crs = 4326)
 
 usethis::use_data(slr_map_cln_data, overwrite = TRUE)
