ffd4bbad9571641af05abd55ccbd1f4d03084f02
AnnaClaire Marley <AnnaClaire.Marley@erg.com>
2025-09-03 11:33:14 -0700

    updated methods for aligning observed and projected data

--- data-raw/slr_plot_mod_all.R
+++ data-raw/slr_plot_mod_all.R
@@ -5,31 +5,42 @@ slr_plot_obs_raw <- readr::read_csv(file.path(config::get("slr_path"), "sea-leve
   janitor::clean_names()
 slr_plot_mod_all_raw <- readr::read_csv(file.path(config::get("slr_path"), "SLR_TF U.S. Sea Level Projections.csv"), skip = 17)
 
-
 # Clean projected data ----------------------------------------------------
 
 slr_plot_mod_cln <- slr_plot_mod_all_raw %>%
   clean_slr_mod_data() %>%
-  dplyr::filter(noaa_name == "GMSL") %>%
-  dplyr::select(-noaa_name, -lat, -long)
+  dplyr::filter(noaa_name == "GMSL")
 
 # Align projected with observed -------------------------------------------
 
-slr_plot_mod_all <- chain_slr_data(
-  obs_data = slr_plot_obs_raw,
-  mod_data = slr_plot_mod_cln,
-  obs_col = noaa_adjusted_sea_level_inches)
+# get the average observed from 2005 to 2020 value to offset the projections data
+obs_av_05_20 <- slr_plot_obs_raw %>%
+  dplyr::select(year, noaa_adjusted_sea_level_inches) %>%
+  dplyr::filter(year %in% c(2005:2020)) %>%
+  dplyr::summarize(obs_av = mean(noaa_adjusted_sea_level_inches)) %>%
+  dplyr::pull()
+
+# Get the projected 2020 value
+mod_2020 <- slr_plot_mod_cln %>%
+  dplyr::filter(year == 2020) %>%
+  dplyr::mutate(slr_in_av = slr_in/2) %>%  # get the average of 2005 (0) and 2020
+  dplyr::mutate(shift_obs_2020 = obs_av_05_20 - slr_in_av) %>%
+  dplyr::select(scenario, shift_obs_2020)
+
+slr_plot_mod_adj <- dplyr::left_join(slr_plot_mod_cln, mod_2020, by = c("scenario")) %>%
+  dplyr::mutate(slr_in = slr_in + shift_obs_2020) %>%
+  dplyr::select(scenario, year, slr_in)
 
 
 # Add model ranges --------------------------------------------------------
 
 # SLR proj mid
-slr_proj_mid <- slr_plot_mod_all %>%
+slr_proj_mid <- slr_plot_mod_adj %>%
   dplyr::filter(scenario %in% c("0.5 - MED", "1.0 - MED")) %>%
   dplyr::mutate(scenario = ifelse(scenario == "0.5 - MED", "Lower sea level rise", "Higher sea level rise"))
 
 # SLR proj range
-slr_proj_range <- slr_plot_mod_all %>%
+slr_proj_range <- slr_plot_mod_adj %>%
   dplyr::filter(scenario %in% c("0.5 - LOW", "0.5 - HIGH", "1.0 - LOW", "1.0 - HIGH")) %>%
   dplyr::mutate(bound = ifelse(stringr::str_detect(scenario, "LOW"), "lower_bound", "upper_bound")) %>%
   dplyr::mutate(scenario = ifelse(stringr::str_detect(scenario, "0.5"), "Lower sea level rise",
@@ -39,16 +50,8 @@ slr_proj_range <- slr_plot_mod_all %>%
 # Combine back together
 slr_plot_mod_all <- dplyr::left_join(slr_proj_mid, slr_proj_range, by = c("year", "scenario"))
 
-# model hindcast
-slr_hindcast <- slr_plot_mod_all %>%
-  dplyr::filter(year <= 2020) %>%
-  dplyr::mutate(scenario = "Model Hindcast") %>%
-  dplyr::distinct()
-
 # add model hindcast back into slr proj
 slr_plot_mod_all <- slr_plot_mod_all %>%
-  dplyr::filter(year >= 2020) %>%
-  rbind(., slr_hindcast) %>%
   dplyr::mutate(scenario_area = paste(scenario, " range"))
 
 usethis::use_data(slr_plot_mod_all, overwrite = TRUE)
