7057eeaafc1724f54d009e523a6451b678362c2d
AnnaClaire Marley <AnnaClaire.Marley@erg.com>
2025-09-10 14:31:58 -0700

    updated run dev parameters

--- dev/run_dev.R
+++ dev/run_dev.R
@@ -1,9 +1,8 @@
 # Set options here
-options(golem.app.prod = FALSE) # TRUE = production mode, FALSE = development mode
+options(golem.app.prod = TRUE) # TRUE = production mode, FALSE = development mode
 
 # Comment this if you don't want the app to be served on a random port
 options(shiny.port = httpuv::randomPort(),
-        shiny.reactlog = TRUE,
         highcharter.rjson = TRUE)
 
 # Detach all loaded packages and clean your environment
