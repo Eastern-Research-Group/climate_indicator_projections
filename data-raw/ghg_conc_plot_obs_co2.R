6a408c3f77cdcd41927992cee61f10b5e9dc12db
AnnaClaire Marley <AnnaClaire.Marley@erg.com>
2025-09-16 17:45:13 +0100

    Changed “Barrow” to “Utqiagvik (Barrow)” for consistency with a change made to another EPA indicator recently.

--- data-raw/ghg_conc_plot_obs_co2.R
+++ data-raw/ghg_conc_plot_obs_co2.R
@@ -1,6 +1,8 @@
 ## code to prepare `ghg_conc_plot_obs_co2` dataset goes here
 ghg_conc_plot_obs_co2 <- readr::read_csv(file.path(config::get("ghg_path"), "ghg-concentrations_fig-1.csv"), skip = 6,
                                          col_types = readr::cols(.default = readr::col_character())) %>%
-  cln_ghg_obs()
+  cln_ghg_obs() %>%
+  # Change name for consistency with a change we made to another EPA indicator recently
+  dplyr::mutate(source = ifelse(source == "Barrow", "Utqiagvik (Barrow)", source))
 
 usethis::use_data(ghg_conc_plot_obs_co2, overwrite = TRUE)
