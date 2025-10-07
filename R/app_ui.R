59c08fd49d26432c7d1ca7e609fb3c5502ba54f8
Sydney Merrill <Sydney.Merrill@erg.com>
2025-10-07 17:53:07 -0400

    Added Length of Growing Season text

--- R/app_ui.R
+++ R/app_ui.R
@@ -73,7 +73,12 @@ app_ui <- function(request) {
                      title = "Length of Growing Season",
                      maps = mod_grow_season_map_ui("grow_season_map_1"),
                      timeseries = mod_grow_season_plot_ui("grow_season_plot_1"),
-                     summary = render_summary(),
+                     summary = render_summary(
+                       intro=read_app_text("grow_season/text_intro.html"),
+                       background=read_app_text("grow_season/text_background.html"),
+                       key_points=read_app_text("grow_season/text_key_points.html"),
+                       sources_and_methods=read_app_text("grow_season/text_sources.html")
+                     ),
                      tech_doc = render_tech_doc(
                        identification=read_app_text("grow_season/td_identification.html"),
                        data_sources=read_app_text("grow_season/td_data_sources.html"),
