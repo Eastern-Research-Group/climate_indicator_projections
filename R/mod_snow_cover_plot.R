d9de70be735735248195fe9a80ea51c720cac28b
Paola Felix <paola.felix@erg.com>
2025-10-08 00:09:20 -0700

    added plot caption and data source text

--- R/mod_snow_cover_plot.R
+++ R/mod_snow_cover_plot.R
@@ -22,6 +22,8 @@ mod_snow_cover_plot_ui <- function(id) {
     render_timeseries_page(
       title="",
       timeseries=shinycssloaders::withSpinner(highcharter::highchartOutput(ns("plot"))),
+      data_source=read_app_text("snow_cover/plot_caption_data_source.html"),
+      caption=read_app_text("snow_cover/plot_caption_text.html")
     )
 
   )
