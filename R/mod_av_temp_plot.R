51fd0d21deb43a7d2cc6f2a23fc39f43ec3f2f4b
Paola Felix <paola.felix@erg.com>
2025-10-07 22:37:30 -0700

    added plot caption and data sources text

--- R/mod_av_temp_plot.R
+++ R/mod_av_temp_plot.R
@@ -12,6 +12,8 @@ mod_av_temp_plot_ui <- function(id) {
   render_timeseries_page(
     title="",
     timeseries=shinycssloaders::withSpinner(highcharter::highchartOutput(ns("plot"))),
+    data_source=read_app_text("av_temp/plot_caption_data_source.html"),
+    caption=read_app_text("av_temp/plot_caption_text.html")
   )
 }
 
